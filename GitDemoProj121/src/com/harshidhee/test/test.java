80ad8b2cf0d76eacdb919604963720edef672930
user <user@user-PC>
2020-04-10 13:41:44 +0530

    Dev2 updated proj121

--- GitDemoProj121/src/com/harshidhee/test/test.java
+++ GitDemoProj121/src/com/harshidhee/test/test.java
@@ -1,5 +1,12 @@
 package com.harshidhee.test;
 
-public class test {
+import com.harshidhee.bean.DateTest;
 
+public class test {
+			
+	
+	public static void main(String[] args) {
+		DateTest dt=new DateTest();
+		System.out.println(dt.getDate());
+	}
 }
