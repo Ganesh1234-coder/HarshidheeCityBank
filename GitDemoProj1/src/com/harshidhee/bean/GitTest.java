87409436bf99325cfd71b233a0152f85ed6579f9
user <user@user-PC>
2020-04-10 13:22:33 +0530

    TL put the initial prj in centeral rep

---
+++ GitDemoProj1/src/com/harshidhee/bean/GitTest.java
@@ -0,0 +1,11 @@
+package com.harshidhee.bean;
+
+import java.util.Date;
+
+public class GitTest {
+	
+	public Date getSystemDate() {
+		return new Date();
+	}
+
+}
---
+++ GitDemoProj1/src/com/harshidhee/test/TestClass.java
@@ -0,0 +1,5 @@
+package com.harshidhee.test;
+
+public class TestClass {
+
+}
